46c6b30457203f6cb7e2758cdacd22807a40f9e9
Law, Jonathan <jonathan.law@chemours.com>
2020-05-13 12:59:29 -0400

    Pause on model development for now, working on data cleaning first

--- nn_model.jl
+++ nn_model.jl
@@ -1,6 +1,15 @@
-using Flux
-using Flux.Optimise: ADAM, train!
+# using Flux
+# using Flux.Optimise: ADAM, train!
+using TextAnalysis
+using CSV
 
+df = CSV.read("data/imdb_dataset.csv")
+
+as_binary(x) = Int(x == "positive")
+df.sentiment = map(as_binary, df.sentiment)
+
+
+"""
 input_size = 2
 epochs = 10
 
@@ -12,13 +21,13 @@ model = Chain(
 X = [[1, 3], [2,4],[3,6]]
 Y = [[5],[6],[7]]
 
-data = Flux.Data.DataLoader(hcat(X...), hcat(Y...))
+train_data = Flux.Data.DataLoader()
+data = zip(hcat(X...), hcat(Y...))
 
 loss(x,y) = Flux.mse(model(x), y)
 opt = ADAM()
 
-for e in epochs
-    train!(loss, Flux.params(model), data, opt)
-end
+@epochs 10 train!(loss, Flux.params(model), data, opt)
 
 println(model(X))
+"""
