fd8d62aa9794e21a198a99fc1c84388e16f8d981
Law, Jonathan <jonathan.law@chemours.com>
2020-05-18 11:00:19 -0400

    Fixed data input; silenced output for preprocess tasks; changed model to sigmoid output, loss to binary crossentropy

--- nn_model.jl
+++ nn_model.jl
@@ -1,5 +1,7 @@
 using Flux
+using Flux: @epochs
 using Flux.Optimise: ADAM, train!
+using Flux.Data: DataLoader
 using TextAnalysis
 using CSV
 
@@ -16,30 +18,35 @@ end
 
 
 as_binary(x) = Int(x == "positive")
-df.sentiment = map(as_binary, df.sentiment)
+df.sentiment = map(as_binary, df.sentiment);
 
-doc_list = map(preprocess, df.review)
-corpus = Corpus(doc_list)
+doc_list = map(preprocess, df.review);
+corpus = Corpus(doc_list);
 
-update_lexicon!(corpus)
-m = DocumentTermMatrix(corpus)
+update_lexicon!(corpus);
+m = DocumentTermMatrix(corpus);
 
-tf = Matrix(tf_idf(m))
-
-train_data = zip(tf, df.sentiment)
+tf = Matrix(tf_idf(m));
+data = DataLoader(transpose(tf), hcat(df.sentiment...), shuffle=true, batchsize=64)
 
 input_size = size(tf, 2)
 epochs = 10
 
 model = Chain(
     Dense(input_size, 10, relu),
-    Dense(10, 1),
+    Dense(10, 1, sigmoid),
 )
 
+function loss_all(data, model)
+    l = 0f0
+    for (x, y) in data
+        l += Flux.binarycrossentropy(model(x), y)
+    end
+    l / length(data)
+end
 
-loss(x,y) = Flux.mse(model(x), y)
+loss(x,y) = Flux.binarycrossentropy(model(x), y)
 opt = ADAM()
+evalcb = () -> @show(loss_all(data, model))
 
-train!(loss, Flux.params(model), train_data, opt)
-
-println(model(X))
+@epochs epochs train!(loss, Flux.params(model), data, opt, cb=evalcb)
