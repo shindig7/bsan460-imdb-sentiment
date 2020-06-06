dc0a2e1dcfef387567edf0073281b5fecfac6a48
shindig7 <jal457@drexel.edu>
2020-06-06 10:29:03 -0400

    Updated according to experimentation results

--- nn_model.py
+++ nn_model.py
@@ -6,10 +6,20 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    f1_score,
+    accuracy_score,
+    confusion_matrix,
+    roc_auc_score,
+    balanced_accuracy_score,
+)
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras import Input
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+import matplotlib.pyplot as plt
+
 
 wnl = WordNetLemmatizer()
 
@@ -42,8 +52,6 @@ def create_model(input_size, layer_size, dropout_rate=0.5):
                 kernel_initializer="uniform",
             ),
             Dropout(dropout_rate),
-            Dense(layer_size, activation="relu", kernel_initializer="uniform"),
-            Dropout(dropout_rate),
             Dense(1, activation="sigmoid"),
         ]
     )
@@ -63,17 +71,51 @@ def main():
         X, y, train_size=0.75, stratify=y
     )
 
-    model = create_model(X.shape[1], 25)
+    model = create_model(X.shape[1], 5)
+
+    epochs = 100
+
+    callbacks = [
+        EarlyStopping(patience=5, monitor="val_loss", mode="min", verbose=1),
+        ModelCheckpoint(
+            filepath="models/model.{epoch:02d}-{val_loss:.3f}.h5",
+            monitor="val_loss",
+            mode="min",
+            save_best_only=True,
+            verbose=1,
+        ),
+    ]
 
     history = model.fit(
         train_x,
         vstack(train_y),
         validation_data=(test_x, test_y),
-        epochs=50,
-        batch_size=128,
+        epochs=epochs,
+        batch_size=32,
+        callbacks=callbacks,
     )
 
-    print(history.history)
+    pred = model.predict_classes(test_x)
+
+    print("===============Metrics===============")
+    print(f"## F1 Score: {f1_score(test_y, pred)}")
+    print(f"## Accuracy: {accuracy_score(test_y, pred)}")
+    print(f"## Confusion Matrix: \n{confusion_matrix(test_y, pred)}")
+    print(f"## Balanced Accuracy: {balanced_accuracy_score(test_y, pred)}")
+    print(f"## ROC AUC Score: {roc_auc_score(test_y, pred)}")
+    print("=====================================")
+
+    plt.figure(figsize=((15, 10)))
+    e = len(history.history["loss"])
+    plt.title("Model Loss")
+    plt.plot(range(1, e + 1), history.history["loss"], "b.")
+    plt.plot(range(1, e + 1), history.history["val_loss"], "r")
+    plt.axvline(x=3)
+
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend(labels=["Train Loss", "Val Loss"])
+    plt.show()
 
 
 if __name__ == "__main__":
